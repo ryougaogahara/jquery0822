c711e018baf5cb32577d3ea55ad1e9b469612409
ryougaogahara <157794652+ryougaogahara@users.noreply.github.com>
2024-08-22 14:51:47 +0900

    Add files via upload

---
+++ 24_resize-header/js/script.js
@@ -0,0 +1,13 @@
+$(function() {
+    // ブラウザーをスクロール
+    $(window).scroll(function() {
+        // スクロール量が300pxを超えているかをチェック
+        if($(window).scrollTop() > 300) {
+            // 超えている場合：header要素にsmallクラスを追加
+            $("header").addClass("small");
+        }else {
+            // 超えていない場合：header要素からsmallクラスを削除
+            $("header").removeClass("small");
+        }
+    });
+});
